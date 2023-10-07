d1e93838eac42d2de541564707a5a336b9131b1e
ricardiobraga <rcardiobraga@gmail.com>
2023-10-07 17:26:14 -0300

    criando o projeto para exemplo de tipos de variaveis

---
+++ java-anatomia-classes/src/edu/ricardo/anatomiadaclasse/App.java
@@ -0,0 +1,7 @@
+package edu.ricardo.anatomiadaclasse;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Nesse  Capítulo, além das instruções de como utilizar e nomear variáveis, foi ensinado sobre estruturas de pasta. ");
+    }
+}
